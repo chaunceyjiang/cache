a839945104ee561d301e32bff9537fe6121e6174
chaunceyjiang <chaunceyiang@gmail.com>
2020-03-24 13:58:39 +0800

    增加http服务端

---
+++ cmd/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"dcache"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var db = map[string]string{
+	"Tom":  "630",
+	"Jack": "589",
+	"Sam":  "567",
+}
+
+func main() {
+	dcache.NewGroup("source", 2<<10, dcache.GetterFunc(func(key string) ([]byte, error) {
+		log.Println("[cache] search key", key)
+		if v, ok := db[key]; ok {
+			return []byte(v), nil
+		}
+		return nil, fmt.Errorf("%s not exist", key)
+	}))
+	addr := "0.0.0.0:9999"
+	peers := dcache.NewHTTPPool(addr)
+	log.Println("cache is running at", addr)
+	//http.Handle("/", peers)
+	log.Fatal(http.ListenAndServe(addr, peers))
+}
---
+++ http.go
@@ -0,0 +1,57 @@
+// 分布式缓存需要实现节点间通信
+package dcache
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const defaultBasePath = "/_cache/"
+
+type HTTPPool struct {
+	self     string //记录自身地址
+	basePath string // 通信地址前缀
+}
+
+func NewHTTPPool(self string) *HTTPPool {
+	return &HTTPPool{
+		self:     self,
+		basePath: defaultBasePath,
+	}
+}
+
+func (p *HTTPPool) Log(format string, v ...interface{}) {
+	log.Printf("[Cache Server %s] %s", p.self, fmt.Sprintf(format, v...))
+}
+
+func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !strings.HasPrefix(r.URL.Path, p.basePath) {
+		panic("HTTPPool serving unexpect path..." + r.URL.Path)
+	}
+	p.Log("%s %s", r.Method, r.URL.Path)
+	// /<basepath>/<groupname>/<key> required
+	parts := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
+	if len(parts) != 2 {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	groupName, key := parts[0], parts[1]
+	group := GetGroup(groupName)
+	if group == nil {
+		// 本机没有这个缓存group
+		http.Error(w, "no such group", http.StatusNotFound)
+		return
+	}
+
+	view, err := group.Get(key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/octet-stream") // 二进制流
+	w.Write(view.ByteSlice())
+}
